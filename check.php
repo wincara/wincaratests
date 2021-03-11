bade660331fd79435b534aa3fbe93cbddfb9c9e4
wincara <73772160+wincara@users.noreply.github.com>
2021-03-11 10:17:00 +0300

    Add files via upload

---
+++ check.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h1>WE are ok</h1>
+<a href="index.php"></a>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>bgsch</title>
+</head>
+<body>
+<h1>TEST DONE. WEBSITE UNDER DEVELOPMENT</h1>
+<a href="check.php">CHECK</a>
+</body>
+</html>
